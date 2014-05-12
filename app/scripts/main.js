bdadaefd665fd77c9fbf8b0c6150b1c64be25d43
João Netto <hi@joaonetto.me>
2014-05-12 11:20:50 -0300

    Proposing another home page layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
-console.log('\'Allo \'Allo!');
+"use strict";
+$(function () {
+    $(window).on('resize load', function() {
+        $('.cover-picture').css('height', $(this).innerHeight());
+    });
+});
